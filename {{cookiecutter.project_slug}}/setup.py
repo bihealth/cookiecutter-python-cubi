cf6252fc7828134e9cf316d6f4252b8eb3539020
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2020-01-28 06:53:21 +0100

    md => rst

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -23,10 +23,10 @@ def parse_requirements(path):
     return requirements
 
 
-with open("README.md") as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open("HISTORY.md") as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 test_requirements = parse_requirements("requirements/test.txt")
